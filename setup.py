1d0f7eb320c410af48f52394e888eed76d171bf9
Marcin Wierzbiński <marcin119a@gmail.com>
2019-11-08 11:32:26 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='formula_convert',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/satproject/formula_convert',
+      author='Sat Project TEAM',
+      author_email='sat@satproject.com',
+      license='GPL',
+      packages=['formula_convert'],
+      zip_safe=False)
