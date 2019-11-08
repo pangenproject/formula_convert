5dfada2ab195b578accc866806a51c2b12c848a1
Marcin Wierzbiński <marcin119a@gmail.com>
2019-11-08 11:21:59 +0100

    Create formulate.py

---
+++ formulate.py
@@ -0,0 +1,32 @@
+cnf = [[1, 2], [1,-3], [2, 4], [-3, -4], [-1, -4]]
+
+import numpy
+
+X = 0
+Y = 1
+
+def formulate(cnfs, N):
+    qubo = numpy.zeros(shape=(N,N))
+    const = 0
+    for cnf in cnfs:
+        i, j = abs(cnf[X])-1, abs(cnf[Y])-1
+        if cnf[X] > 0 and cnf[Y] > 0:
+            const = const + 1
+            qubo[i,i] = qubo[i,i] -1
+            qubo[j,j] = qubo[j,j] -1
+            qubo[i,j] = qubo[i,j] + 1/2
+            qubo[j,i] = qubo[j,i] + 1/2
+        elif cnf[X] > 0 and cnf[Y] < 0:
+            qubo[j,j] = qubo[j,j] + 1
+            qubo[i,j] = qubo[i,j] + 1/2
+            qubo[j,i] = qubo[j,i] + 1/2
+        elif cnf[X] < 0 and cnf[Y] > 0:
+            qubo[i,i] = qubo[i,i] + 1
+            qubo[i,j] = qubo[i,j] - 1/2
+            qubo[j,i] = qubo[j,i] - 1/2
+        elif cnf[X] < 0 and cnf[Y] < 0:
+            qubo[i,j] = qubo[i,j] + 1/2
+            qubo[j,i] = qubo[j,i] + 1/2
+    return qubo
+
+formulate(cnf, 4)
