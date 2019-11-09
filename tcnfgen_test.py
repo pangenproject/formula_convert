aada6ec6b14c95aa26aa583eb07bedc7428b1151
Mateusz Zugaj <57489682+matimz@users.noreply.github.com>
2019-11-09 12:46:27 +0100

    Update tcnfgen_test.py

--- tcnfgen_test.py
+++ tcnfgen_test.py
@@ -3,10 +3,10 @@ N= 100
 M=100
 C=[]
 for i in range(0,N):
-
+    s = 0
     for j in range(0,M):
-        s = 0
-        if pycosat.solve(tcnfgen(N-int(j/2),N))=='UNSAT':
+        alfa = 2*i
+        if pycosat.solve(tcnfgen(alfa,N))=='UNSAT':
             s = s+1
     C.append([i,(M-s)/M])
 plt.plot([x[0] for x in C],[y[1] for y in C])
