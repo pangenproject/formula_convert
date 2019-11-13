96d5ddfa14bff16fde63a035571749a3c1b087b0
Marcin Wierzbiński <marcin119a@gmail.com>
2019-11-14 00:11:38 +0100

    Update plot_charts.py

--- plot_charts.py
+++ plot_charts.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.genfromtxt('pycosatvswq.csv', delimiter=',', skip_header=10,
-                     skip_footer=10, names=['x', 'y', 'z'])
+                     skip_footer=10, names=['alpha', 'wq', 'pycosat'])
 fig = plt.figure()
 
 ax1 = fig.add_subplot(111)
 
-ax1.set_title("alpga")
-ax1.set_xlabel('alpga')
-ax1.set_ylabel('% ')
+ax1.set_title("100 variables 2-SAT")
+ax1.set_xlabel('clause Density')
+ax1.set_ylabel('% satisfiability ')
 
-ax1.plot(data['x'], data['y'], color='r', label='the data')
-ax1.plot(data['x'], [1-x for x in data['z']], color='b', label='the data')
+ax1.plot(data['alpha'], data['wq'], color='r', label='wq')
+ax1.plot(data['alpha'], [1-x for x in data['pycosat']], color='b', label='pycosat')
 
 
 fig.show()
