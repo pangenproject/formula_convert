8e3fdf46df0cc9b271dcffda90294cb41b4efcd0
Marcin Wierzbiński <marcin119a@gmail.com>
2019-11-26 00:04:48 +0100

    refractor

--- example.py
+++ example.py
@@ -3,23 +3,23 @@ import numpy as np
 import pycosat as pysat
 import csv
 
-N = 20
+N = 100
 REPEAT = 20
 list_results = []
 
 
-def dw_solve(formulas):
+def dw_solve(formulas, qubo):
     dw_instance = wq.opt()
-    dw_instance.qubo = q
+    dw_instance.qubo = qubo
     dw_instance.dwavetoken = "DEV-283ae48a24e86ddd7654f48e223202dfd4a26ce0"
     spins_conf = dw_instance.dw()
     bin_vector_dw = [1 if spin == 1 else 0 for spin in spins_conf]
 
     return bin_vector_dw
 
-def wq_solve(formulas):
+def wq_solve(formulas, qubo):
     wildqat_instance = wq.opt()
-    wildqat_instance.qubo = q
+    wildqat_instance.qubo = qubo
 
     bin_vector_wildqat = wildqat_instance.sa()
 
@@ -28,29 +28,31 @@ def wq_solve(formulas):
 
 
 for i in range(1, N):
-    d, w, p = 0, 0, 0
+    d = 0
+    w = 0 
+    p = 0
     alfa = 2 * i
 
     for j in range(0, REPEAT):
 
         cnf_formulas = tcnfgen(alfa, N)
 
-        q, const = formulate(cnf_formulas, N)
+        qubo, const = formulate(cnf_formulas, N)
 
 
-        bin_vector_dw = dw_solve(formulas=cnf_formulas)
-        if np.dot(bin_vector_dw, np.dot(q, bin_vector_dw)) + const == 0:
+        bin_vector_dw = dw_solve(cnf_formulas, qubo)
+        if np.dot(bin_vector_dw, np.dot(qubo, bin_vector_dw)) + const == 0:
             d = d + 1
 
-        bin_vector_wildqat = wq_solve(formulas=cnf_formulas)
+        bin_vector_wildqat = wq_solve(cnf_formulas, qubo)
 
-        if np.dot(bin_vector_wildqat, np.dot(q, bin_vector_wildqat)) + const == 0:
+        if np.dot(bin_vector_wildqat, np.dot(qubo, bin_vector_wildqat)) + const == 0:
             w = w + 1
 
         if pysat.solve(cnf_formulas) == 'UNSAT':
             p = p + 1
 
-    results = [alfa/N, d/REPEAT, (REPEAT-w)/REPEAT, (REPEAT-p)/REPEAT]
+    results = [alfa/N, d/REPEAT, w/REPEAT, (REPEAT-p)/REPEAT]
     list_results.append(results)
     myFile = open('compare.csv', 'a')
     with myFile:
